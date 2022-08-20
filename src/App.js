1bc56246cb93deb27b257e65371a4c13729fed87
antomuangigitau <muangigitau@gmail.com>
2022-08-20 10:31:33 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './styles/index.css';
+import Nav from './components/header/Nav';
+import Banking from './components/main/Banking';
+import Easy from './components/main/Easy';
+import Latest from './components/main/Latest';
+import Contact from './components/footer/Contact';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app__body'>
+      <header className='header__container'>
+        <Nav />
       </header>
+      <main className='main__section'>
+        <section className='main__banking__section'>
+          <Banking />
+        </section>
+        <section className='choose__easy__bank'>
+          <Easy />
+        </section>
+        <section className='latest__articles'>
+          <Latest />
+        </section>
+      </main>
+      <footer className='footer'>
+        <Contact />
+      </footer>
     </div>
   );
 }
---
+++ src/components/main/Easy.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Features from '../main/Features';
+import { items } from '../data';
+const header = 'Why choose Easybank?';
+const paragraph = `We leverage Open Banking to turn your bank account into your financial hub. Control 
+  your finances like never before.
+`;
+const Easy = () => {
+  return (
+    <div className='easy__bank'>
+      <div className='inner__easy__bank'>
+        <h2>{header}</h2>
+        <p>{paragraph}</p>
+      </div>
+      <div className='flex__inner'>
+        {items.map((item) => {
+          return <Features key={item.id} {...item} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Easy;

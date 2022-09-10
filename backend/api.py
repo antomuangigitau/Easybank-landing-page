6f8fd10e42280d698dcd71ca719d428bd3ff981d
Gitahi <gitahi109@gmail.com>
2022-09-10 22:38:43 +0300

    add api features

--- backend/api.py
+++ backend/api.py
@@ -1,31 +1,96 @@
-from flask import Flask
-from flask_restful import Resource, Api, abort
-from .models import setup_db, Customer
+from flask import Flask, request
+from flask_restful import Resource, Api, abort, reqparse
+from models import setup_db, Customer, Account
+from typing import Union
 
-database_path = 'sqlite:///./bank.db'
+database_path = 'sqlite:////tmp/banks.db'
 app = Flask(__name__)
 api = Api(app)
 setup_db(app, database_path)
 
+# parser = reqparse.RequestParser()
+# parser.add_argument()
+
+# Helper functions to avoid redundancy when querying from the database
 def find_customer(customer_id):
     query = Customer.query.filter_by(id=customer_id).first()
+
+    # Turn this method into a global method to be able to use it for all queries
+    # in short, rename it to find() and do the necessary work
 
     if query:
         return query.format()
     else:
         abort(404, message="Customer {} doesn't exist".format(customer_id))
 
+def get_all(resource: Union[Customer, Account]):
+    all_query = resource.query.all()
 
-class Customer(Resource):
+    if all_query:
+        return [query.format() for query in all_query]
+    else:
+        abort(404, message="Resource not found")
+
+def delete_resource(resource: Union[Customer, Account], resource_id):
+    query = resource.query.filter_by(id=resource_id).first()
+
+    if query:
+        query.delete_record()
+    else:
+        abort(404, message="{} with id: {} does not exist".format(query.__class__, resource_id))
+
+# the actual resources to be used
+class EachCustomer(Resource):
     def get(self, customer_id):
         customer = find_customer(customer_id)
         return customer
 
     def put(self, customer_id):
         customer = find_customer(customer_id)
-        //Not thru
+        # //Not thru
 
-api.add_resource(Customer, '/customer/<customer_id>')
+    def delete(self, customer_id):
+        delete_resource(Customer, customer_id)
+
+
+class CustomerList(Resource):
+    def get(self):
+        customers = get_all(Customer)
+        return customers
+
+    def post(self):
+        # args = parser.parse_args()
+        input_data = request.get_json()
+
+        if input_data:
+            try:
+                first_name = input_data.get('first_name')
+                last_name = input_data.get('last_name')
+                phone = input_data.get('phone')
+                email = input_data.get('email')
+                date_joined = input_data.get('date')
+                username = input_data.get('username')
+                password = input_data.get('password')
+
+                new_customer = Customer(first_name=first_name,
+                                        last_name=last_name,
+                                        phone=phone,
+                                        email=email,
+                                        date_of_joining=date_joined,
+                                        username=username,
+                                        password=password #Will def hash it later
+                                        )
+                new_customer.insert_to_db()
+
+            except Exception:
+                abort(400, message='Bad request from your end')
+            
+        else:
+            abort(404, message='Resource not found')
+
+
+api.add_resource(EachCustomer, '/customer/<customer_id>')
+api.add_resource(CustomerList, '/customers')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- backend/models.py
+++ backend/models.py
@@ -9,44 +9,92 @@ def setup_db(app, db_path):
     db.app = app
     db.init_app(app)
     db.create_all()
+    synthesise_db()
 
 
-#Customer Model
 class Customer(db.Model):
     id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
     phone = Column(Integer)
     email = Column(String)
-    date_of_joining = Column(DateTime)
+    # date_of_joining = Column(DateTime)
     username = Column(String)
     password = Column(String)
+    accounts = db.relationship('Account', backref='customer', lazy=True)
     
-    def format(self):
+    def __init__(self,
+    			first_name,
+    			last_name,
+    			phone,
+    			email,
+    			# date_of_joining,
+    			username,
+    			password) -> None:
+
+    	self.first_name = first_name
+    	self.last_name = last_name
+    	self.phone = phone
+    	self.email = email
+    	# self.date_of_joining = date_of_joining
+    	self.username = username
+    	self.password = password
+
+    def format(self) -> dict:
         return {
             'first_name': self.first_name,
             'last_name': self.last_name,
             'phone': self.phone,
             'email': self.email,
-            'join_date': self.date_of_joining,
+            # 'join_date': self.date_of_joining,
             'username': self.username
         }
 
+    def delete_record(self):
+    	db.session.delete(self)
+    	db.session.commit()
+
+    def insert_to_db(self) -> None:
+    	db.session.add(self)
+    	db.session.commit()
+
+   	# def delete(self):
+   	# 	db.session.delete(self)
+   	# 	db.session.commit()
+
 class Account(db.Model):
-    id = Column(Integer, primary_kry=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     date_opened = Column(DateTime)
-    customer_id = Column(Integer, FK) #Not thru
+    customer_id = Column(Integer, db.ForeignKey("customer.id"), nullable=False) #Not thru
+    transactions = db.relationship('Transaction', backref='account', lazy=True)
+
+class Transactiontype(db.Model):
+	# __tablename__ = 'transactiontype'
+
+    id = Column(Integer, primary_key=True)
+    description = Column(String)
+    transactions = db.relationship('Transaction', backref='transactiontype', lazy=True)
+
 
 class Transaction(db.Model):
     id = Column(Integer, primary_key=True)
     date = Column(DateTime)
     amount_transacted = Column(Integer)
-    account_id = Column(Integer, ForeignKey("Account.id")) #Not thru
-    transaction_type = Column(Integer, FOreignKey("Transaction.id"))
+    account_id = Column(Integer, db.ForeignKey("account.id"), nullable=False) #Not thru
+    transaction_type = Column(Integer, db.ForeignKey("transactiontype.id"), nullable=False)
 
-class Transaction_Type(db.Model):
-    id = Column(Integer, primary_key=True)
-    description = Column(String)
 
-    
+
+def synthesise_db():
+	from datetime import datetime
+
+	customer = Customer(first_name='Donald',
+						last_name='Trump',
+						phone=25473131831,
+						email='trump@potus.gov',
+						# date_of_joining=datetime(2019, 12, 12),
+						username='trump',
+						password='test')
+
+	customer.insert_to_db()
